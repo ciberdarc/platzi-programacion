d3f446bedad696d9fd28446c8baba28d8ba03d08
Alexis Araujo <ala1289@gmail.com>
2017-01-02 08:45:33 -0600

    Se agrego una caja de texto y un boton para calcular las lineas que se dibujara en el canvas

--- canvas/dibujo.js
+++ canvas/dibujo.js
@@ -1,24 +1,12 @@
+var texto = document.getElementById("texto_lineas");
+var boton = document.getElementById("botoncito");
+
+boton.addEventListener("click", dibujoPorClick );
+
 var d = document.getElementById("dibujito");
+var ancho = d.width;
 var lienzo = d.getContext("2d");
-var lineas = 30;
-var l = 0;
-var yi, xf;
-var colorcito = "red"
 
-for(l=0; l < lineas; l++)
-  {
-    yi = 10 * l;
-    xf = 10 * (l + 1);
-    dibujarLinea (colorcito, 300, yi, xf, 0);
-    dibujarLinea (colorcito, 0, yi, xf, 300);
-    console.log("Linea " + l);
-  }
-
-dibujarLinea(colorcito, 1, 1, 1, 300);
-dibujarLinea(colorcito, 1, 299, 299, 299);
-
-dibujarLinea(colorcito, 2, 1, 299, 1);
-dibujarLinea(colorcito, 299, 1, 299, 299);
 
 function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal) 
 {
@@ -28,6 +16,31 @@ function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal)
   lienzo.lineTo(xfinal, yfinal);
   lienzo.stroke();
   lienzo.closePath();
+}
+
+function dibujoPorClick ()
+{
+
+  var lineas = parseInt(texto.value);
+  var l = 0;
+  var yi, xf;
+  var colorcito = "red"
+  var espacio = ancho / lineas;
+
+  for(l=0; l < lineas; l++)
+    {
+      yi = espacio * l;
+      xf = espacio * (l + 1);
+      dibujarLinea (colorcito, 300, yi, xf, 0);
+      dibujarLinea (colorcito, 0, yi, xf, 300);
+      console.log("Linea " + l);
+    }
+
+  dibujarLinea(colorcito, 1, 1, 1, 300);
+  dibujarLinea(colorcito, 1, 299, 299, 299);
+
+  dibujarLinea(colorcito, 2, 1, 299, 1);
+  dibujarLinea(colorcito, 299, 1, 299, 299);
 }
 
 
